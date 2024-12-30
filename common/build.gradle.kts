271915bdc0e3c0c8b4edd00f4bc62e71cdc730f8
Liang <757437150@qq.com>
2024-12-30 10:33:30 +0800

    #modify

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
+    kotlin("jvm") version "2.1.0"
     `java-library`
     `maven-publish`
 }
+val javaVersion = JavaVersion.VERSION_17.toString()
 
 group = "com.ola.learning"
 version = "1.0-SNAPSHOT"
@@ -11,8 +13,9 @@ repositories {
 }
 
 dependencies {
+    implementation(kotlin("stdlib"))
     implementation("org.projectlombok:lombok:1.18.24")
-//    annotationProcessor("org.projectlombok:lombok:1.18.24")
+    annotationProcessor("org.projectlombok:lombok:1.18.24")
 }
 
 publishing {
@@ -21,7 +24,7 @@ publishing {
             from(components["java"])
             groupId = "com.ola"
             artifactId = "learn-commons"
-            version = "1.0.0"
+            version = "1.0.2"
         }
     }
     repositories {
@@ -36,6 +39,18 @@ publishing {
     }
 }
 
+tasks.withType<JavaCompile> {
+    sourceCompatibility = javaVersion
+    targetCompatibility = javaVersion
+    options.compilerArgs.add("-proc:full")
+}
+
+//tasks.register("compileJava", JavaCompile::class) {
+//    source = fileTree("src/main/java")
+//    classpath = sourceSets["main"].compileClasspath
+////    destinationDir = file("build/classes")
+//    options.compilerArgs.add("-proc:full")
+//}
 //tasks.test {
 //    useJUnitPlatform()
 //}
